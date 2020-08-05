b868145aef1c18ce22626d517b9454c8ee9965ad
Mahavishnu <selvamvishnu25@gmail.com>
2020-08-05 11:58:59 +0530

    Update dbconnection.php

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "detsdb");
+$con=mysqli_connect("us-cdbr-east-02.cleardb.com", "46eb9ce4", "b2a869e0807ee8", "heroku_fedc9f7c9269d2d");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
